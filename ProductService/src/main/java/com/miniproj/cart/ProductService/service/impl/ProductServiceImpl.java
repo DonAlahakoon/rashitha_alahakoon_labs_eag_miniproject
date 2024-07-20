fd1081e7ad5c7824432fa333c42270295d51ab0e
rala4354 <rashitha.alahakoon@sysco.com>
2024-07-20 09:49:58 +0530

    Added logging to productServiceImpl

--- ProductService/src/main/java/com/miniproj/cart/ProductService/service/impl/ProductServiceImpl.java
+++ ProductService/src/main/java/com/miniproj/cart/ProductService/service/impl/ProductServiceImpl.java
@@ -6,43 +6,50 @@ import com.miniproj.cart.ProductService.exception.ResourceNotFoundException;
 import com.miniproj.cart.ProductService.dto.mapper.ProductMapper;
 import com.miniproj.cart.ProductService.repository.ProductRepository;
 import com.miniproj.cart.ProductService.service.ProductService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
+@AllArgsConstructor
 @Service
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
     @Override
     public ProductDto addNewProduct(ProductDto productDto) {
+        log.debug("Adding new product: {}",productDto);
         Product product = ProductMapper.mapToProduct(productDto);
         Product savedProduct = productRepository.save(product);
+        log.debug("product saved: {}",savedProduct);
         return ProductMapper.mapToProductDto(savedProduct);
     }
 
     @Override
     public ProductDto getProductById(int productId) {
+        log.debug("Fetching product by id: {}",productId);
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product not exist for given id: " +productId));
+        log.debug("product fetched: {}", product);
         return ProductMapper.mapToProductDto(product);
     }
 
     @Override
     public List<ProductDto> getAllProducts() {
+        log.debug("Fetching all the products");
         List<Product> products = productRepository.findAll();
+        log.debug("products fetched: {}",products);
         return products.stream().map(ProductMapper::mapToProductDto)
                 .collect(Collectors.toList());
     }
 
     @Override
     public ProductDto updateProduct(int productId, ProductDto productDto) {
+        log.debug("Updating product with id: {}, data: {}", productId, productDto);
         Product product  = productRepository.findById(productId)
                 .orElseThrow(()-> new ResourceNotFoundException("Product not exist for given id: "+productId));
 
@@ -54,14 +61,18 @@ public class ProductServiceImpl implements ProductService {
         product.setCreatedAt(productDto.getCreatedAt());
         product.setUpdatedAt(productDto.getUpdatedAt());
 
-        return ProductMapper.mapToProductDto(product);
+        Product updatedProductObj = productRepository.save(product);
+        log.debug("product updated: {}",updatedProductObj);
+        return ProductMapper.mapToProductDto(updatedProductObj);
     }
 
     @Override
     public void deleteProduct(int productId) {
+        log.debug("Deleting product with id: {}",productId);
         Product product = productRepository.findById(productId)
                 .orElseThrow(()-> new ResourceNotFoundException("Product not exist for given id: "+productId));
         productRepository.deleteById(productId);
+        log.debug("product soft-deleted with id: {}",productId);
 
     }
 }
